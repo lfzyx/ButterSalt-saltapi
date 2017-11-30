d946f7df550fd90911750a1122270c618bdfa5d4
lfzyx <lfzyx.me@gmail.com>
2017-11-30 11:26:02 +0800

    add function unittest

---
+++ tests/saltapi_test.py
@@ -0,0 +1,32 @@
+import unittest
+from buttersalt_saltapi import saltapi
+from config import config
+
+
+salt = saltapi.SaltApi(baseurl=config['default'].SALT_API,
+                       username=config['default'].SALT_USERNAME,
+                       password=config['default'].SALT_PASSWORD)
+
+
+class ServerTestCase(unittest.TestCase):
+
+    def test_login(self):
+        re = salt.login()
+        self.assertTrue(re)
+
+    def test_get_keys(self):
+        re = salt.get_keys()
+        self.assertIsInstance(re.get('minions'), list)
+
+    def test_get_jobs(self):
+        re = salt.get_jobs()
+        self.assertIsInstance(re, dict)
+
+    def test_get_minions(self):
+        re = salt.get_minions()
+        self.assertIsInstance(re, dict)
+
+    def test_get_stats(self):
+        re = salt.get_stats()
+        self.assertIsInstance(re, dict)
+
